376fe75afff03f3efc9e523f4f2e6d0bc89c9f2c
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-16 16:25:47 +0530

    Create HRank2.cpp

---
+++ HRank2.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a,b,c,sum=0;
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<sum<<endl; 
+    return 0;
+}

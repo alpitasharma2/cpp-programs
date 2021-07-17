f5d0faf07547f59b23fc88b1d6f24270a335b6a6
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-17 21:38:31 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n,val=1;
+    cout << "Enter n" << endl;
+    cin >> n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<val;
+            val++;
+            
+        }
+       
+        cout<<endl;
+
+    }
+
+    return 0;
+}

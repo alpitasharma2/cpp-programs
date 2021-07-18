f0c8bc6a0f1254a50f45573f29148511ce9f42ee
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-18 15:53:40 +0530

    Update pattern3.cpp

--- pattern3.cpp
+++ pattern3.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n,k;
+    cout << "enter n"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        k=i;
+        for(j=1;j<=i;j++)
+        {
+          cout<<k;
+          k++;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}

c0564e5e395161e4a642eec01024a2fb3e187d18
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-18 16:11:59 +0530

    Update pattern4.cpp

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n,k;
+    cout << "enter n"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        k=i;
+        for(j=i; j<n; j++)
+        {
+            cout<<" ";
+        }
+
+        for(j=1;j<=i;j++)
+        {
+          cout<<k;
+          k++;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
 

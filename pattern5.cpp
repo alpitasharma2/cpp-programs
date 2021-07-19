8e7f9e4b418513040f3d1c8c5eea3db20fa56447
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-19 13:08:49 +0530

    Update pattern5.cpp

--- pattern5.cpp
+++ pattern5.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n,k;
+    cout << "enter n"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+
+        for(j=1; j<=n; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
 

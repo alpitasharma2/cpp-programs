cf8467df13bbf120e846e957eecdaedcdcd996c6
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-19 15:02:37 +0530

    Update pattern6.cpp

--- pattern6.cpp
+++ pattern6.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n,k;
+    cout << "enter n"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+
+        for(j=i; j>=1; j--)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
 
